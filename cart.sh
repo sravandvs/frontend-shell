c19f9023ff9d9294bdab2f77ddc487fd70a61203
sravandvs <sravandvs6@gmail.com>
2024-10-21 21:55:39 +0530

    cart shell

--- cart.sh
+++ cart.sh
@@ -7,13 +7,13 @@ cp cart.service /etc/systemd/system/cart.service
 useradd roboshop
 
 mkdir /app
-
-curl  -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
 cd /app
 unzip /tmp/cart.zip
 
 cd /app
 npm install
 
+systemctl daemon-reload
 systemctl enable cart
 systemctl restart cart
