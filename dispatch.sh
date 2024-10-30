b2915dac4ce4fca4551bd9407a9fc76e2cf924e0
sravandvs <sravandvs6@gmail.com>
2024-10-30 13:28:20 +0530

    Modifications

--- dispatch.sh
+++ dispatch.sh
@@ -2,23 +2,23 @@ source common.sh
 app_name=dispatch
 
 echo -e "$color Copy Dispatch Service File $no_color"
-cp dispatch.service /etc/systemd/system/dispatch.service
+cp dispatch.service /etc/systemd/system/dispatch.service &>>log_file
 echo $?
 
 echo -e "$color Install GoLang $no_color"
-dnf install golang -y
+dnf install golang -y &>>log_file
 echo $?
 
 app_prerequisites
 
 echo -e "$color Copy Download Application Dependencies $no_color"
-go mod init dispatch
-go get
-go build
+go mod init dispatch &>>log_file
+go get &>>log_file
+go build &>>log_file
 echo $?
 
 echo -e "$color Start Application Service $no_color"
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+systemctl daemon-reload &>>log_file
+systemctl enable dispatch &>>log_file
+systemctl restart dispatch &>>log_file
 echo $?
