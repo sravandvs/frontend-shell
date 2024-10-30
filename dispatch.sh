e089c605dfc311ca4a0648e27efe551a33c7a705
sravandvs <sravandvs6@gmail.com>
2024-10-30 12:26:54 +0530

    Function

--- dispatch.sh
+++ dispatch.sh
@@ -1,4 +1,5 @@
 source common.sh
+app_name=dispatch
 
 echo -e "$color Copy Dispatch Service File $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
@@ -6,20 +7,7 @@ cp dispatch.service /etc/systemd/system/dispatch.service
 echo -e "$color Install GoLang $no_color"
 dnf install golang -y
 
-echo -e "$color Add Application User $no_color"
-useradd roboshop
-
-echo -e "$color  Create Application Directory $no_color"
-#rm -rf /app
-mkdir /app
-
-echo -e "$color Download Application Directory $no_color"
-curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
-cd /app
-
-echo -e "$color Extract Application Content $no_color"
-unzip /tmp/dispatch.zip
-cd /app
+app_prerequisites
 
 echo -e "$color Copy Download Application Dependencies $no_color"
 go mod init dispatch
--- payment.sh
+++ payment.sh
@@ -1,19 +1,18 @@
+source common.sh
+app_name=payment
+
+echo -e '$color Copy Payment Service file $no_color'
 cp payment.service /etc/systemd/system/payment.service
 
+echo -e '$color Install Python $no_color'
 dnf install python3 gcc python3-devel -y
 
-useradd roboshop
+app_prerequisites()
 
-mkdir /app
-
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
-cd /app
-unzip /tmp/payment.zip
-
-cd /app
+echo -e "$color Download Application Dependencies $no_color"
 pip3 install -r requirements.txt
 
+echo -e "$color Start Application Service $no_color"
 systemctl daemon-reload
-
 systemctl enable payment
 systemctl start payment
