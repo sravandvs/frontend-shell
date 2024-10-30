27a66e4332d6ea7d4d352df5fe380b0e8fbd1541
sravandvs <sravandvs6@gmail.com>
2024-10-30 13:01:20 +0530

    Modifications

--- common.sh
+++ common.sh
@@ -4,15 +4,20 @@ no_color = "\e[0m"
 app_prerequisites() {
 echo -e "$color Add Application User $no_color"
 useradd roboshop
+echo $?
 
 echo -e "$color  Create Application Directory $no_color"
 rm -rf /app
 mkdir /app
+echo $?
 
 echo -e "$color Download Application Directory $no_color"
 curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
+echo $?
+
 cd /app
 
 echo -e "$color Extract Application Content $no_color"
 unzip /tmp/$app_name.zip
+echo $?
 }
--- dispatch.sh
+++ dispatch.sh
@@ -3,9 +3,11 @@ app_name=dispatch
 
 echo -e "$color Copy Dispatch Service File $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
+echo $?
 
 echo -e "$color Install GoLang $no_color"
 dnf install golang -y
+echo $?
 
 app_prerequisites
 
@@ -13,8 +15,10 @@ echo -e "$color Copy Download Application Dependencies $no_color"
 go mod init dispatch
 go get
 go build
+echo $?
 
 echo -e "$color Start Application Service $no_color"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl start dispatch
+echo $?
